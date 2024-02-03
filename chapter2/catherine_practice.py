ee7d08219313696f48555070ea2c3a684e377ea0
Catherine-Russell <catherinerussell123@hotmail.co.uk>
2024-02-03 23:25:14 +0000

    completes chapter 2 revision

---
+++ chapter2/catherine_practice.py
@@ -0,0 +1,10 @@
+example_int = [3, 5, 6, 7, 2, 1]
+example_string = ['cat', 'tom', 'lizzie', 'lydia', 'will', 'joe', 'jack']
+
+ex_dict = {'name': 'Guido van Rossum',
+           'nationality': 'Dutch',
+           'favorite_programming_language': 'Python',
+           'birthday': '31 January',
+           'favourite_colour': 'Purple'
+           }
+
